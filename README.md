08f690cb0a90426978b101ce69fb2f015ed5fd35
Craig Hesling <craig@hesling.com>
2022-12-08 17:07:25 -0500

    Expand the tool to search Google and Cloudflare ranges
    The tool now supports:
    * Amazon AWS
    * Google Services
    * Google Cloud
    * Cloudflare

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Amazon AWS IP Lookup
+# Cloud Services IP Lookup
 
-This tool uses the published
-[AWS IP Ranges](https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html)
-list to check if the given IP is contained in one of the ranges.
+This tool checks if the given IP exists in any of the published IP ranges from
+[Amazon AWS][AWS IP Ranges], [Google Services][Google Services IP Ranges],
+[Google Cloud][Google Cloud IP Ranges], or [Cloudflare][Cloudflare IP Ranges].
 
-Once the ip ranges JSON is downloaded, the search for the range occurs offline.
+Once the IP ranges JSON is downloaded from the respective cloud provider, the
+search for the encapsulating IP range occurs offline.
 
 ## Usage:
 
@@ -12,13 +13,47 @@ Once the ip ranges JSON is downloaded, the search for the range occurs offline.
 lookup.py <lookup_ip_address>
 ```
 
-## Example
+## Example 1
 
 ```shell
 ./lookup.py 44.242.161.7
 ```
 
 Outputs:
-> {'ip_prefix': '44.224.0.0/11', 'region': 'us-west-2', 'service': 'AMAZON', 'network_border_group': 'us-west-2'}
-> {'ip_prefix': '44.224.0.0/11', 'region': 'us-west-2', 'service': 'EC2', 'network_border_group': 'us-west-2'}
-> {'ip_prefix': '44.242.161.6/31', 'region': 'us-west-2', 'service': 'KINESIS_VIDEO_STREAMS', 'network_border_group': 'us-west-2'}
+> AWS: {'ip_prefix': '44.224.0.0/11', 'region': 'us-west-2', 'service': 'AMAZON', 'network_border_group': 'us-west-2'}
+> AWS: {'ip_prefix': '44.224.0.0/11', 'region': 'us-west-2', 'service': 'EC2', 'network_border_group': 'us-west-2'}
+> AWS: {'ip_prefix': '44.242.161.6/31', 'region': 'us-west-2', 'service': 'KINESIS_VIDEO_STREAMS', 'network_border_group': 'us-west-2'}
+
+## Example 2
+
+```shell
+./lookup.py 8.8.8.8
+```
+
+Outputs:
+> Google Service: {'ipv4Prefix': '8.8.8.0/24'}
+
+# Example 3
+
+```shell
+./lookup.py 35.231.19.128
+```
+
+Outputs:
+> Google Service: {'ipv4Prefix': '35.224.0.0/12'}
+> Google Cloud: {'ipv4Prefix': '35.231.0.0/16', 'service': 'Google Cloud', 'scope': 'us-east1'}
+
+## Example 4
+
+```shell
+./lookup.py 2a06:98c1:50::ac40:20b3
+```
+
+Outputs:
+> Cloudflare: 2a06:98c0::/29
+
+
+[AWS IP Ranges]: https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
+[Google Services IP Ranges]: https://support.google.com/a/answer/10026322?hl=en
+[Google Cloud IP Ranges]: https://support.google.com/a/answer/10026322?hl=en
+[Cloudflare IP Ranges]: https://www.cloudflare.com/ips
--- lookup.py
+++ lookup.py
@@ -1,14 +1,101 @@
 #!/usr/bin/env python3
 
-"""A tool to check if a given IP exists in the published AWP IP range.
-
-See https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html.
+"""A tool that checks if a given IP exists in the published IP ranges for AWS,
+Google Services, Google Cloud, or Cloudflare.
 """
 
 import argparse
 import ipaddress
 import requests
 import sys
+
+
+def lookup_aws(lookup_ip) -> bool:
+    """Check the Amazon AWS IP range for lookup_ip.
+
+    See https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html.
+    """
+
+    found = False
+    aws_ip_ranges = requests.get(
+        'https://ip-ranges.amazonaws.com/ip-ranges.json').json()
+    for prefix in aws_ip_ranges['prefixes']:
+        ip_prefix = ipaddress.ip_network(prefix['ip_prefix'])
+        # The IP may exist in multiple ranges, so search all.
+        if lookup_ip in ip_prefix:
+            found = True
+            print('AWS:', prefix)
+    return found
+
+
+def lookup_google_services(lookup_ip) -> bool:
+    """Check the Google Services IP range for lookup_ip.
+
+    See https://support.google.com/a/answer/10026322?hl=en.
+    """
+
+    found = False
+    goog_service_ip_ranges = requests.get(
+        'https://www.gstatic.com/ipranges/goog.json').json()
+    for prefix in goog_service_ip_ranges['prefixes']:
+        ip_prefix = None
+        if 'ipv4Prefix' in prefix:
+            ip_prefix = ipaddress.ip_network(prefix['ipv4Prefix'])
+        if 'ipv6Prefix' in prefix:
+            assert ip_prefix == None, "Found Google service IP range with ipv4Prefix and ipv6Prefix"
+            ip_prefix = ipaddress.ip_network(prefix['ipv6Prefix'])
+
+        # The IP may exist in multiple ranges, so search all.
+        if lookup_ip in ip_prefix:
+            found = True
+            print("Google Service:", prefix)
+    return found
+
+
+def lookup_google_cloud(lookup_ip) -> bool:
+    """Check the Google Cloud IP range for lookup_ip.
+
+    See https://support.google.com/a/answer/10026322?hl=en.
+    """
+
+    found = False
+    goog_cloud_ip_ranges = requests.get(
+        'https://www.gstatic.com/ipranges/cloud.json').json()
+    for prefix in goog_cloud_ip_ranges['prefixes']:
+        ip_prefix = None
+        if 'ipv4Prefix' in prefix:
+            ip_prefix = ipaddress.ip_network(prefix['ipv4Prefix'])
+        if 'ipv6Prefix' in prefix:
+            assert ip_prefix == None, "Found Google cloud IP range with ipv4Prefix and ipv6Prefix"
+            ip_prefix = ipaddress.ip_network(prefix['ipv6Prefix'])
+
+        # The IP may exist in multiple ranges, so search all.
+        if lookup_ip in ip_prefix:
+            found = True
+            print("Google Cloud:", prefix)
+    return found
+
+
+def lookup_cloudflare(lookup_ip) -> bool:
+    """Check the Cloudflare IP range for lookup_ip.
+
+    See https://www.cloudflare.com/ips/.
+    """
+
+    found = False
+    cloudflare_ipv4_ranges = requests.get(
+        "https://www.cloudflare.com/ips-v4").text.splitlines()
+    cloudflare_ipv6_ranges = requests.get(
+        "https://www.cloudflare.com/ips-v6").text.splitlines()
+    cloudflare_ip_ranges = cloudflare_ipv4_ranges + cloudflare_ipv6_ranges
+    for prefix in cloudflare_ip_ranges:
+        ip_prefix = ipaddress.ip_network(prefix)
+
+        # The IP may exist in multiple ranges, so search all.
+        if lookup_ip in ip_prefix:
+            found = True
+            print("Cloudflare:", prefix)
+    return found
 
 
 def main(argv: list) -> int:
@@ -21,16 +108,16 @@ def main(argv: list) -> int:
     opts = parser.parse_args(argv)
     lookup_ip = opts.lookup_ip
 
-    ip_ranges = requests.get(
-        'https://ip-ranges.amazonaws.com/ip-ranges.json').json()
-
     found = False
-    for prefix in ip_ranges['prefixes']:
-        ip_prefix = ipaddress.ip_network(prefix['ip_prefix'])
-        # The IP may exist in multiple ranges, so search all.
-        if lookup_ip in ip_prefix:
-            found = True
-            print(prefix)
+
+    if lookup_aws(lookup_ip):
+        found = True
+    if lookup_google_services(lookup_ip):
+        found = True
+    if lookup_google_cloud(lookup_ip):
+        found = True
+    if lookup_cloudflare(lookup_ip):
+        found = True
 
     return 1 if found else 0
 
